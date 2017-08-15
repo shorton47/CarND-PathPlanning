3c051bf643bc83277a6e1cb862fc4079737dc419
SWH <shorton@outlook.com>
2017-08-15 09:03:49 -0700

    Working. Got spline operational.

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,16 @@
+//----------
+//
+//
+// Structure of Main.CPP:
+//    Init
+//    In-line Functions
+//    Helper Methods - General & Geometry
+//    Main Method
+//        Read Waypoints for Simulator Track
+//        WebSocket Message Handler Functions including onMessage (which is key)
+//        Start Messaging handling
+//
+//----------
 #include <fstream>
 #include <math.h>
 #include <uWS/uWS.h>
@@ -8,16 +21,21 @@
 #include "Eigen-3.3/Eigen/Core"
 #include "Eigen-3.3/Eigen/QR"
 #include "json.hpp"
+#include "spline.h"              // Copyright (C) 2011, 2014 Tino Kluge (ttk448 at gmail.com)
 
-using namespace std;
 
 // for convenience
 using json = nlohmann::json;
+using namespace std;
 
-// For converting back and forth between radians and degrees.
+// In-line functions for converting back and forth between radians and degrees.
 constexpr double pi() { return M_PI; }
 double deg2rad(double x) { return x * pi() / 180; }
 double rad2deg(double x) { return x * 180 / pi(); }
+
+//---
+// Helper Methods
+//---
 
 // Checks if the SocketIO event has JSON data.
 // If there is data the JSON object in string format will be returned,
@@ -33,6 +51,10 @@ string hasData(string s) {
   }
   return "";
 }
+
+//---
+// Utility Methods Section
+//---
 
 double distance(double x1, double y1, double x2, double y2)
 {
@@ -159,45 +181,56 @@ vector<double> getXY(double s, double d, vector<double> maps_s, vector<double> m
 
 }
 
+//---
+// Main method for Message processing
+//---
 int main() {
-  uWS::Hub h;
+    
+    //---
+    // NOTE: Key constants for main
+    //---
+    string map_file_ = "../data/highway_map.csv";  // Waypoint map to read from
+    double const FULL_TRACK_S = 6945.554;          // s value before wrapping around the track back to 0 (meters)
+    int lane = 1;                                  // Define lane (0=Far Left, 1=Center, 2=Far Right)
+    double ref_velocity = 49.00;                   // Reference velocity for the car (start at max)
+    //---
+    
+    uWS::Hub h;
 
-  // Load up map values for waypoint's x,y,s and d normalized normal vectors
-  vector<double> map_waypoints_x;
-  vector<double> map_waypoints_y;
-  vector<double> map_waypoints_s;
-  vector<double> map_waypoints_dx;
-  vector<double> map_waypoints_dy;
+    //
+    // Section to load values for Waypoint's x,y,s and d normalized normal vectors
+    //
+    vector<double> map_waypoints_x;
+    vector<double> map_waypoints_y;
+    vector<double> map_waypoints_s;
+    vector<double> map_waypoints_dx;
+    vector<double> map_waypoints_dy;
 
-  // Waypoint map to read from
-  string map_file_ = "../data/highway_map.csv";
-  // The max s value before wrapping around the track back to 0
-  double max_s = 6945.554;
+    // Open & load Waypoint map file for Simulator track
+    ifstream in_map_(map_file_.c_str(), ifstream::in);
+    string line;
+    while (getline(in_map_, line)) {
+        istringstream iss(line);
+        double x;
+        double y;
+        float s;
+        float d_x;  // x Normal component of d
+        float d_y;  // y Normal component of d
+        iss >> x;
+        iss >> y;
+        iss >> s;
+        iss >> d_x;
+        iss >> d_y;
+        map_waypoints_x.push_back(x);
+        map_waypoints_y.push_back(y);
+        map_waypoints_s.push_back(s);
+        map_waypoints_dx.push_back(d_x);
+        map_waypoints_dy.push_back(d_y);
+    } // end Waypoint map load
 
-  ifstream in_map_(map_file_.c_str(), ifstream::in);
-
-  string line;
-  while (getline(in_map_, line)) {
-  	istringstream iss(line);
-  	double x;
-  	double y;
-  	float s;
-  	float d_x;
-  	float d_y;
-  	iss >> x;
-  	iss >> y;
-  	iss >> s;
-  	iss >> d_x;
-  	iss >> d_y;
-  	map_waypoints_x.push_back(x);
-  	map_waypoints_y.push_back(y);
-  	map_waypoints_s.push_back(s);
-  	map_waypoints_dx.push_back(d_x);
-  	map_waypoints_dy.push_back(d_y);
-  }
-
-  h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
-                     uWS::OpCode opCode) {
+    
+  h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy,&lane, &ref_velocity]
+              (uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
     // The 2 signifies a websocket event
@@ -213,8 +246,11 @@ int main() {
         string event = j[0].get<string>();
         
         if (event == "telemetry") {
-          // j[1] is the data JSON object
           
+            //
+            // Section #1 : Get all data returned from Simulator
+            //
+            // j[1] is the data JSON object
         	// Main car's localization Data
           	double car_x = j[1]["x"];
           	double car_y = j[1]["y"];
@@ -226,23 +262,194 @@ int main() {
           	// Previous path data given to the Planner
           	auto previous_path_x = j[1]["previous_path_x"];
           	auto previous_path_y = j[1]["previous_path_y"];
-          	// Previous path's end s and d values 
+          	
+            // Previous path's end s and d values
           	double end_path_s = j[1]["end_path_s"];
           	double end_path_d = j[1]["end_path_d"];
 
           	// Sensor Fusion Data, a list of all other cars on the same side of the road.
           	auto sensor_fusion = j[1]["sensor_fusion"];
+            //---
+            
+            //
+            // Section #2. - Prediction /  Proceess vehicle data
+            //
+            
+            //
+            // Section #3 - Behavior / Determine Vehicle Situation
+            //
+            
+            //
+            // Section #4 - Trajectory Generation Take Vehicle Action
+            //
+            
+            // Simulator returns previous car path at each timestep
+            int prev_size = previous_path_x.size();
+            
+            
+            // 1st thing is to check if car in front (this might go in "prediction or behavior section
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            // Widely spaced waypoints, evenly spaced at 30m (anchor points)
+            vector<double> ptsx;  // Anchor points for trajectory spline
+            vector<double> ptsy;
+            
+            double ref_x = car_x;
+            double ref_y = car_y;
+            double ref_yaw = deg2rad(car_yaw);
+            //double car_yaw_r = deg2rad(car_yaw);
+            
+            if (prev_size < 2) {
+            
+                // Path tangent to current angle of the car
+                double prev_car_x = car_x - cos(car_yaw);  // Need to convert to radians?
+                double prev_car_y = car_y - sin(car_yaw);
+                
+                //double prev_car_x = car_x - cos(car_yaw_r);  // Need to convert to radians?
+                //double prev_car_y = car_y - sin(car_yaw_r);
+            
+                ptsx.push_back(prev_car_x);
+                ptsx.push_back(car_x);
+                
+                ptsy.push_back(prev_car_y);
+                ptsy.push_back(car_y);
+            
+            
+            } else {
+            
+                ref_x = previous_path_x[prev_size-1];
+                ref_y = previous_path_y[prev_size-1];
+                
+                double ref_x_prev = previous_path_x[prev_size-2];
+                double ref_y_prev = previous_path_y[prev_size-2];
+                ref_yaw = atan2(ref_y-ref_y_prev, ref_x-ref_x_prev);
+                
+                // Use 2 points that make path tangent to the previous path's end points
+                ptsx.push_back(ref_x_prev);
+                ptsx.push_back(ref_x);
+                
+                ptsy.push_back(ref_y_prev);
+                ptsy.push_back(ref_y);
+            
+            }
+            
+            // Translate to Frenet Space & create more points
+            vector<double> next_wp0 = getXY(car_s+30, (2+4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+            vector<double> next_wp1 = getXY(car_s+60, (2+4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+            vector<double> next_wp2 = getXY(car_s+90, (2+4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+            
+            ptsx.push_back(next_wp0[0]);
+            ptsx.push_back(next_wp1[0]);
+            ptsx.push_back(next_wp2[0]);
+            
+            ptsy.push_back(next_wp0[1]);
+            ptsy.push_back(next_wp1[1]);
+            ptsy.push_back(next_wp2[1]);
+            
+            // Transfer points into car reference frame (keeps spline well behaved, easier math. Class hint)
+            for (int i=0; i<ptsx.size(); i++ ) {
+            
+                // Translate, then rotate to 0 degrees. Will reverse at end: rotate and translate...
+                double shift_x = ptsx[i] - ref_x;
+                double shift_y = ptsy[i] - ref_y;
+                
+                ptsx[i] = (shift_x*cos(0-ref_yaw) - shift_y*sin(0-ref_yaw));
+                ptsy[i] = (shift_x*sin(0-ref_yaw) + shift_y*cos(0-ref_yaw));
+            }
 
-          	json msgJson;
-
+            tk::spline s;
+            
+            s.set_points(ptsx,ptsy);  // Create spline from anchor points
+            
+            
+            
+            
+            //---
+            // TODO: define a path made up of (x,y) points that the car will visit sequentially every .02 seconds
           	vector<double> next_x_vals;
           	vector<double> next_y_vals;
 
-
-          	// TODO: define a path made up of (x,y) points that the car will visit sequentially every .02 seconds
-          	msgJson["next_x"] = next_x_vals;
+            for (int i=0; i<previous_path_x.size(); i++) {
+                next_x_vals.push_back(previous_path_x[i]);
+                next_y_vals.push_back(previous_path_y[i]);
+            }
+            
+            // Generate points ahead of car along spline (in local/car coordinates) TODO Change x,y to car_, car_y
+            double target_x = 30.0;  // horizon meters ahead
+            double target_y = s(target_x);
+            double target_dist = sqrt((target_x*target_x) + (target_y*target_y));
+            
+            double x_add_on = 0;  // Start at vehicle x=0
+            for (int i=1; i<=50-previous_path_x.size(); i++) {
+            
+                double N = (target_dist/(.02*ref_velocity/2.24)); // convert from mph to m/s
+                double x_point = x_add_on + (target_x/N);
+                double y_point = s(x_point);
+                
+                x_add_on = x_point;
+                
+                double x_ref = x_point;
+                double y_ref = y_point;
+                
+                // Rotate back to "normal" e earlier rotation
+                x_point = x_ref*cos(ref_yaw) - y_ref*sin(ref_yaw);
+                y_point = x_ref*sin(ref_yaw) + y_ref*cos(ref_yaw);
+            
+                x_point += ref_x;
+                y_point += ref_y;
+                
+                
+                next_x_vals.push_back(x_point);
+                next_y_vals.push_back(y_point);
+            
+            
+            
+            
+            
+            
+            
+            }
+            
+            /*
+            // Second cut at car movement
+            double dist_inc = 0.35;  // .5  (meters)
+            double const LANE_WIDTH = 4.0;
+            int const NUM_LANES = 3;
+            for(int i = 0; i < 40; i++) // # of path points
+            {
+                // Create a path for car in Frenet
+                double next_s = car_s + (i+1)*dist_inc;  // Start 1 point in front of car
+                double next_d = LANE_WIDTH*1.50;  // Center Lane
+                
+                vector<double> xy = getXY(next_s, next_d, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+                next_x_vals.push_back(xy[0]);
+                next_y_vals.push_back(xy[1]);
+                
+                
+                // 1st pass - Convert to Map (x,y)
+                //next_x_vals.push_back(car_x+(dist_inc*i)*cos(deg2rad(car_yaw)));
+                //next_y_vals.push_back(car_y+(dist_inc*i)*sin(deg2rad(car_yaw)));
+            }
+            */
+            
+            
+            //
+          	//  Section #5. Compose response message & send to Simulator
+            //
+            json msgJson;
+            msgJson["next_x"] = next_x_vals;
           	msgJson["next_y"] = next_y_vals;
-
           	auto msg = "42[\"control\","+ msgJson.dump()+"]";
 
           	//this_thread::sleep_for(chrono::milliseconds(1000));
@@ -272,24 +479,30 @@ int main() {
   });
 
   h.onConnection([&h](uWS::WebSocket<uWS::SERVER> ws, uWS::HttpRequest req) {
-    std::cout << "Connected!!!" << std::endl;
+    std::cout << "Connected!" << std::endl;
   });
 
   h.onDisconnection([&h](uWS::WebSocket<uWS::SERVER> ws, int code,
                          char *message, size_t length) {
     ws.close();
-    std::cout << "Disconnected" << std::endl;
+    std::cout << "Disconnected!" << std::endl;
   });
 
-  int port = 4567;
-  if (h.listen(port)) {
-    std::cout << "Listening to port " << port << std::endl;
-  } else {
-    std::cerr << "Failed to listen to port" << std::endl;
+    
+    // Start of Main message handling
+    int port = 4567;
+    if (h.listen(port)) {
+        cout << "Path Planning message handling start: listening on port=" << port << endl;
+    } else {
+        cout << "Path Planning message handler failed: listening on port=" << port << " Exiting." << endl;
+        cerr << "Path Planning message handler failed: listening on port=" << port << " Exiting." << endl;
     return -1;
-  }
-  h.run();
-}
+    }
+    
+    h.run();   // run Websocket message handler
+    return 0;  // TODO: find exit conditions of run
+    
+} // Main
 
 
 
