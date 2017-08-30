30b11edbd39dfdca1947bf6dfcdcd44d90dd9d3e
SWH <shorton@outlook.com>
2017-08-29 23:04:25 -0700

    Walked Back util library

--- src/main.cpp
+++ src/main.cpp
@@ -51,12 +51,25 @@
 
 // My support libraries
 //#include "utility"
-#include "utils.hpp"  // My utility library
+//#include "utils.hpp"  // My utility library
 
 // For convenience
 using json = nlohmann::json;
 //using State = SelfDrivingCar::State;
 using namespace std;
+
+
+// In-line functions for converting back and forth between radians and degrees.
+constexpr double pi() { return M_PI; }
+double deg2rad(double x) { return x * pi() / 180; }
+double rad2deg(double x) { return x * 180 / pi(); }
+double meters2miles(double x) { return x * 0.000621371; }
+double miles2meters(double x) { return x * 1609.3440; }
+double mps2mph(double x) { return x * 2.236936292; }
+
+
+
+
 
 //
 // Important Project LevelConstants: Simulator, Track, etc...
